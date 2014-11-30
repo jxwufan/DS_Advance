18285cb31b52316a050f8998b75def5a50fcb5f4
Fan <jxwufan@gmail.com>
2014-11-30 13:56:51 +0800

    Add comment on population.cpp

--- Project1/population.cpp
+++ Project1/population.cpp
@@ -1,22 +1,21 @@
-#include <algorithm>
-#include <iostream>
 #include <cstring>
 #include <cstdio>
-#include <vector>
 #include <map>
 
-using namespace std;
+using namespace std; // For using the fucntions in the namespace std
 
-int const N = 20001;
-char buffer[100];
-char state;
-map<int, int> bitArray[N];
-bool flag;
+int const N = 20001; // Constant for the range of cordinate
+char buffer[100]; // Buffer for input data
+char state; // Signal of operation
+map<int, int> bitArray[N]; // The database of bit
+bool flag; // Mark of EOF
 
 int lowbit(int t)
 {
-    return t & (-t);
+    return t & (-t); 
 }
+// This function will calcuate the number of consective 0 in the last of the binary representation of t
+// For the bit data structure
 
 void modify(int i, int j, int delta)
 {
@@ -24,6 +23,8 @@ void modify(int i, int j, int delta)
         for (int y = j; y < N; y += lowbit(y))
             bitArray[x][y] += delta;
 }
+// Update bit's database
+// Only the entries which related to the modified data will be updated
 
 int sumOfRectangle(int i, int j)
 {
@@ -35,45 +36,49 @@ int sumOfRectangle(int i, int j)
                 sum += bitArray[x][y];
     return sum;
 }
+//This funtion will caculate all the entries which's coordinate is in the rectangle that's down-left coordinate is (1,1) and up-right coordinate is (i,j)
+
+//The three functions will be discussed in the ducument concretely
 
 int query(int x1, int x2, int y1, int y2)
 {
     return sumOfRectangle(x2, y2) - sumOfRectangle(x1 - 1, y2) - sumOfRectangle(x2, y1 - 1) + sumOfRectangle(x1 - 1, y1 - 1);
 }
+//This funtion will caculate all the entries which's coordinate is in the rectangle that's down-left coordinate is (x1,y1) and up-right coordinate is (x2,y2)
 
 int main()
 {
-    flag = false;
+    flag = false; //Set EOF had not came yet
     for (;;)
     {
         for (int i = 1; i < N; i++)
-            bitArray[i].clear();
+            bitArray[i].clear(); //Initialize the bit's database
         for(;;)
         {
-            if (gets(buffer) == NULL) flag = true;
-            if (flag) break;
-            if (strlen(buffer) == 1)
+            if (gets(buffer) == NULL) flag = true; // EOF is coming!!!
+            if (flag) break; // Stop the program
+            if (strlen(buffer) == 1) // A signal is coming
             {
-                if (buffer[0] == 'E') break;
-                state = buffer[0];
+                if (buffer[0] == 'E') break; // End of the test case
+                state = buffer[0]; // Change the input state
             }
             else
-            if (buffer[0] >= '0' && buffer[0] <= '9')
+            if (buffer[0] >= '0' && buffer[0] <= '9') // It's a operation on data
             {
-                int x1 = 0, x2 = 0, y1 = 0, y2 = 0, N = 0;
-                if (state == 'I')
+                int x1 = 0, x2 = 0, y1 = 0, y2 = 0, N = 0; // Initialize some temp variable
+                if (state == 'I') // People is coming in
                 {
-                    sscanf(buffer, "%d%d%d", &x1, &y1, &N);
-                    modify(x1, y1, N);
+                    sscanf(buffer, "%d%d%d", &x1, &y1, &N); // Read the data of operation
+                    modify(x1, y1, N); // Update database
                 }
                 else
                 {
-                    sscanf(buffer, "%d%d%d%d", &x1, &x2, &y1, &y2);
-                    printf("%d\n", query(x1, x2, y1, y2));
+                    sscanf(buffer, "%d%d%d%d", &x1, &x2, &y1, &y2); // Read the data of operation
+                    printf("%d\n", query(x1, x2, y1, y2)); // Output the query result
                 }
             }
         }
-        if (flag) break;
+        if (flag) break; // Stop the program 
     }
     return 0;
 }
